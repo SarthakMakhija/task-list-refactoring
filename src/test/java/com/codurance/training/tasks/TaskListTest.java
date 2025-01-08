d9d05b85e707167561c06752f9fdc4c996b3c5c5
Sarthak Makhija <sarthak.makhija@gmail.com>
2025-01-08 19:16:23 +0530

    Sarthak | Adds a test for TaskList

---
+++ src/test/java/com/codurance/training/tasks/TaskListTest.java
@@ -0,0 +1,35 @@
+package com.codurance.training.tasks;
+
+import org.junit.Test;
+
+import java.io.StringWriter;
+
+import static org.junit.Assert.assertEquals;
+
+public class TaskListTest {
+
+    @Test
+    public void testExecuteWithAdditionOfAProjectContainingOneTask() throws Exception {
+        StringWriter writer = new StringWriter();
+        TaskList taskList = new TaskList(writer);
+        taskList.execute("add project caizin");
+        taskList.execute("add task caizin Task1");
+        taskList.execute("show");
+
+        String expected = "caizin\n" + "[ ] 1: Task1" + "\n";
+        assertEquals(expected, writer.toString());
+    }
+
+    @Test
+    public void testExecuteWithAdditionOfAProjectContainingACoupleOfTasks() throws Exception {
+        StringWriter writer = new StringWriter();
+        TaskList taskList = new TaskList(writer);
+        taskList.execute("add project caizin");
+        taskList.execute("add task caizin Task1");
+        taskList.execute("add task caizin Task2");
+        taskList.execute("show");
+
+        String expected = "caizin\n" + "[ ] 1: Task1" + "\n" + "[ ] 2: Task2" + "\n";
+        assertEquals(expected, writer.toString());
+    }
+}
