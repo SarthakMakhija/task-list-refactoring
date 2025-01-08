5768916cce5d55200e0dfdfe9e6b369e8cfddf08
Sarthak Makhija <sarthak.makhija@gmail.com>
2025-01-08 20:24:57 +0530

    Sarthak | Adds a couple of tests for TaskList

--- src/test/java/com/codurance/training/tasks/TaskListTest.java
+++ src/test/java/com/codurance/training/tasks/TaskListTest.java
@@ -32,4 +32,33 @@ public class TaskListTest {
         String expected = "caizin\n" + "[ ] 1: Task1" + "\n" + "[ ] 2: Task2" + "\n";
         assertEquals(expected, writer.toString());
     }
+
+    @Test
+    public void executeWithAdditionOfAProjectContainingACoupleOfTasksAndCheckOneOfThem() throws Exception {
+        StringWriter writer = new StringWriter();
+        TaskList taskList = new TaskList(writer);
+        taskList.execute("add project caizin");
+        taskList.execute("add task caizin Task1");
+        taskList.execute("add task caizin Task2");
+        taskList.execute("check 1");
+        taskList.execute("show");
+
+        String expected = "caizin\n" + "[x] 1: Task1" + "\n" + "[ ] 2: Task2" + "\n";
+        assertEquals(expected, writer.toString());
+    }
+
+    @Test
+    public void executeWithAdditionOfAProjectContainingACoupleOfTasksAndUncheckOneOfThem() throws Exception {
+        StringWriter writer = new StringWriter();
+        TaskList taskList = new TaskList(writer);
+        taskList.execute("add project caizin");
+        taskList.execute("add task caizin Task1");
+        taskList.execute("add task caizin Task2");
+        taskList.execute("check 1");
+        taskList.execute("uncheck 1");
+        taskList.execute("show");
+
+        String expected = "caizin\n" + "[ ] 1: Task1" + "\n" + "[ ] 2: Task2" + "\n";
+        assertEquals(expected, writer.toString());
+    }
 }
