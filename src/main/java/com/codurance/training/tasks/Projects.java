e441f16663f9af15fd9bbcb1ac176794404b9fa3
Sarthak Makhija <sarthak.makhija@gmail.com>
2025-01-08 20:15:37 +0530

    Sarthak | Refactors Projects, and introduces Project

--- src/main/java/com/codurance/training/tasks/Projects.java
+++ src/main/java/com/codurance/training/tasks/Projects.java
@@ -1,37 +1,50 @@
 package com.codurance.training.tasks;
 
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
-public class Projects extends LinkedHashMap<String, Tasks> {
+public class Projects extends LinkedHashMap<String, Project> {
 
     void addProject(String name) {
-        this.put(name, new Tasks());
+        this.put(name, new Project(name));
     }
 
-    void addTaskToProjectWithName(String project, Task task) {
-        List<Task> projectTasks = this.get(project);
-        if (projectTasks == null) {
+    void addTaskToProjectWithName(String projectName, Task task) {
+        Project project = this.get(projectName);
+        if (project == null) {
             throw new IllegalArgumentException("Unknown project: " + project);
         }
-        projectTasks.add(task);
+        project.addTask(task);
     }
 
     boolean toggleTaskWithId(int id, boolean done) {
-        for (Map.Entry<String, Tasks> project : this.entrySet()) {
-            if (project.getValue().toggleTaskWithId(id, done)) return true;
+        for (Map.Entry<String, Project> project : this.entrySet()) {
+            if (project.getValue().tasks.toggleTaskWithId(id, done)) return true;
         }
         return false;
     }
 
     String format() {
         StringBuilder formatted = new StringBuilder();
-        for (Map.Entry<String, Tasks> project : this.entrySet()) {
+        for (Map.Entry<String, Project> project : this.entrySet()) {
             formatted.append(project.getKey());
             formatted.append("\n");
-            formatted.append(project.getValue().format());
+            formatted.append(project.getValue().tasks.format());
         }
         return formatted.toString();
+    }
+}
+
+class Project {
+    private final String name;
+    final Tasks tasks;
+
+    public Project(String name) {
+        this.name = name;
+        this.tasks = new Tasks();
+    }
+
+    public void addTask(Task task) {
+        this.tasks.add(task);
     }
 }
