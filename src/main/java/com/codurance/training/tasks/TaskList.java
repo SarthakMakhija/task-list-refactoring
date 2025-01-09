509401e25766003bbbe57dd62a4cab60fba39d8f
Sarthak Makhija <sarthak.makhija@gmail.com>
2025-01-09 15:30:53 +0530

    Sarthak | Refactors TaskList, uses Commands

--- src/main/java/com/codurance/training/tasks/TaskList.java
+++ src/main/java/com/codurance/training/tasks/TaskList.java
@@ -4,40 +4,14 @@ import java.io.Writer;
 
 public final class TaskList {
 
-    private final AddTaskCommand addTaskCommand;
-    private final AddProjectCommand addProjectCommand;
-    private final ShowCommand showCommand;
-    private final CheckCommand checkCommand;
-    private final UncheckCommand uncheckCommand;
+    private final Commands commands;
 
     public TaskList(Writer writer) {
-        Projects projects = new Projects();
-        this.addTaskCommand = new AddTaskCommand(projects);
-        this.addProjectCommand = new AddProjectCommand(projects);
-        this.showCommand = new ShowCommand(writer, projects);
-        this.checkCommand = new CheckCommand(writer, projects);
-        this.uncheckCommand = new UncheckCommand(writer, projects);
+        commands = new Commands(writer);
     }
 
     public void execute(String commandLine) throws Exception {
-        CommandDescription commandDescription = new CommandLine(commandLine).parse();
-        switch (commandDescription.getCommandType()) {
-            case ADD_TASK:
-                this.addTaskCommand.execute(commandDescription.getArguments());
-                break;
-            case ADD_PROJECT:
-                this.addProjectCommand.execute(commandDescription.getArguments());
-                break;
-            case SHOW:
-                this.showCommand.execute(commandDescription.getArguments());
-                break;
-            case CHECK:
-                this.checkCommand.execute(commandDescription.getArguments());
-                break;
-            case UNCHECK:
-                this.uncheckCommand.execute(commandDescription.getArguments());
-                break;
-        }
+        commands.execute(new CommandLine(commandLine).parse());
     }
 }
 
