c77a83287c3106e9dfb7b89cba9cd2d5da2834af
Sarthak Makhija <sarthak.makhija@gmail.com>
2025-01-10 12:47:34 +0530

    Sarthak | Adds some notes on Arguments to think about refactoring

--- src/main/java/com/codurance/training/commands/Arguments.java
+++ src/main/java/com/codurance/training/commands/Arguments.java
@@ -5,6 +5,15 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * The class `Arguments` is showing signs of both mutability and immutability.
+ * Signs of mutability:
+ *  - Arguments extends from ArrayList<String> which is mutable.
+ *  - The method `skipOne` creates a new instance of Arguments, which means the author does not want to change the existing collection.
+ *  An instance of Arguments is passed to all the Commands. Given, Arguments extends from ArrayList<String>, it gives all the commands
+ *  the authority to mutate arguments.
+ *  Technically, Arguments should never be mutated once created.
+ */
 public class Arguments extends ArrayList<String> {
 
     static Arguments skipOneAndCreate(String[] parts) {
